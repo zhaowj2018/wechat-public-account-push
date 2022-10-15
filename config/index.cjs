1a4789c601449f8e08af3d9faf8da41737002148
zhaowj2018 <43203712+zhaowj2018@users.noreply.github.com>
2022-10-15 13:37:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -109,7 +109,7 @@ const USER_CONFIG = {
       id: 'oqaJU6un2vRycRnVvl1f1v1Z7Rk0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'wvFAnfm0A_9pc5SGj2ToCoMJhPeHECY70zjV5LwWj64', 
+      useTemplateId: '8vCQUG_8QWc4sJ91Kf6YrIBuGpXQ2LSWq4a14WPKNVQ', 
       // 所在省份或城市，也可以不填
       province: '徐州',
       // 所在城市或县区
@@ -124,7 +124,7 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '老婆', year: '1988', date: '01-05',
+          type: '*生日', name: '小秋', year: '1988', date: '01-05',
         },
         {
           type: '节日', name: '结婚纪念日', year: '2022', date: '09-24',
