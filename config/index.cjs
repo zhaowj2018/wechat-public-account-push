ecd495914c5117266833e41338b446616cdd163e
zhaowj2018 <43203712+zhaowj2018@users.noreply.github.com>
2022-10-15 22:12:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -106,7 +106,7 @@ const USER_CONFIG = {
       name: '小秋',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       // 使用其他通道时，请严格按照各个通道的教程进行填写
-      id: 'oqaJU6un2vRycRnVvl1f1v1Z7Rk0',
+      id: 'oqaJU6lSbQW8lebUP5Roxv2OAejU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
       useTemplateId: 'kgnz0GJ-VzqLPEn94eNwxrvfVUGc2fV95qzuqYBSDJs', 
