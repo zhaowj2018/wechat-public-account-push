2c7bdcb044e0b34a1dc53b65fea99e95739a4cce
zhaowj2018 <43203712+zhaowj2018@users.noreply.github.com>
2022-10-16 13:35:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -109,7 +109,7 @@ const USER_CONFIG = {
       id: 'oqaJU6un2vRycRnVvl1f1v1Z7Rk0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'lrPGNKPuIxXOfL58yXldQtRTVqlW62xrgNaXH6ljpa8', 
+      useTemplateId: '88VCFxPufwDV_F3SV9IMxABTpf2CcYmF743xpAvSlyY', 
       // 所在省份或城市，也可以不填
       province: '江苏',
       // 所在城市或县区
